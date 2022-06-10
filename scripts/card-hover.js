350c81cf76775346006d1b94cd9c33a14e6a3d1c
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-06-10 15:57:00 +0200

    Delete card-hover.js

--- scripts/card-hover.js
+++
@@ -1,7 +0,0 @@
-const cardArea = document.querySelector(".project-grid--card");
-const cardImage = document.querySelector(".project-grid--card--img");
-
-cardArea.addEventListener("mouseover", () => {
-    console.log("Yes");
-    cardImage.style.transform = "scale(.9)";
-});
